33eb8e4cc6e61b0820e19799f6d14afd7aa9d526
maf1978 <marcos.ferrero001@gmail.com>
2023-02-19 23:51:20 -0500

    "first commit"

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  Type = string
+  Name = "us-east-1"
+}
+variable "project_name" {}
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
